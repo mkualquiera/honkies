f24caf8a446ba3a994f74bf666f7be904b434ab1
mkualquiera <ozjuanpa@gmail.com>
2022-08-24 02:47:35 +0000

    loads!

--- worker.py
+++ worker.py
@@ -10,9 +10,7 @@ from tqdm import tqdm, trange
 from itertools import islice
 from einops import rearrange
 from torchvision.utils import make_grid
-import time
 from pytorch_lightning import seed_everything
-import accelerate
 
 import k_diffusion as K
 from ldm.util import instantiate_from_config
@@ -61,97 +59,8 @@ class CFGDenoiser(nn.Module):
         return uncond + (cond - uncond) * cond_scale
 
 
-def do_run(accelerator, device, model, config, opt):
-    from types import SimpleNamespace
-
-    opt = SimpleNamespace(**opt)
-    seed_everything(opt.seed)
-    seeds = torch.randint(-(2**63), 2**63 - 1, [accelerator.num_processes])
-    torch.manual_seed(seeds[accelerator.process_index].item())
-
-    if opt.plms:
-        sampler = PLMSSampler(model)
-    else:
-        sampler = DDIMSampler(model)
-
-    model_wrap = K.external.CompVisDenoiser(model)
-    sigma_min, sigma_max = model_wrap.sigmas[0].item(), model_wrap.sigmas[-1].item()
-
-    os.makedirs(opt.outdir, exist_ok=True)
-    outpath = opt.outdir
-
-    batch_size = opt.n_samples
-    n_rows = opt.n_rows if opt.n_rows > 0 else batch_size
-    if not opt.from_file:
-        prompt = opt.prompt
-        assert prompt is not None
-        data = [batch_size * [prompt]]
-
-    else:
-        print(f"reading prompts from {opt.from_file}")
-        with open(opt.from_file, "r") as f:
-            data = f.read().splitlines()
-            data = list(chunk(data, batch_size))
-
-    sample_path = os.path.join(outpath, "samples")
-    os.makedirs(sample_path, exist_ok=True)
-    base_count = len(os.listdir(sample_path))
-    grid_count = len(os.listdir(outpath)) - 1
-
-    with torch.no_grad():
-        with model.ema_scope():
-            with torch.cuda.amp.autocast():
-                tic = time.time()
-                all_samples = list()
-                for n in trange(
-                    opt.n_iter, desc="Sampling", disable=not accelerator.is_main_process
-                ):
-                    for prompts in tqdm(
-                        data, desc="data", disable=not accelerator.is_main_process
-                    ):
-                        uc = model.get_learned_conditioning(batch_size * [""])
-                        if isinstance(prompts, tuple):
-                            prompts = list(prompts)
-                        c = model.get_learned_conditioning(prompts)
-                        shape = [opt.C, opt.H // opt.f, opt.W // opt.f]
-                        sigmas = model_wrap.get_sigmas(opt.ddim_steps)
-                        x = (
-                            torch.randn([opt.n_samples, *shape], device=device)
-                            * sigmas[0]
-                        )
-                        model_wrap_cfg = CFGDenoiser(model_wrap)
-                        extra_args = {"cond": c, "uncond": uc, "cond_scale": opt.scale}
-                        samples_ddim = K.sampling.sample_lms(
-                            model_wrap_cfg,
-                            x,
-                            sigmas,
-                            extra_args=extra_args,
-                            disable=not accelerator.is_main_process,
-                        )
-                        x_samples_ddim = model.decode_first_stage(samples_ddim)
-                        x_samples_ddim = torch.clamp(
-                            (x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0
-                        )
-                        x_samples_ddim = accelerator.gather(x_samples_ddim)
-
-                        if accelerator.is_main_process and not opt.skip_save:
-                            for x_sample in x_samples_ddim:
-                                x_sample = 255.0 * rearrange(
-                                    x_sample.cpu().numpy(), "c h w -> h w c"
-                                )
-                                Image.fromarray(x_sample.astype(np.uint8)).save(
-                                    os.path.join(sample_path, f"{base_count:05}.png")
-                                )
-                                base_count += 1
-                        all_samples.append(x_samples_ddim)
-
-                toc = time.time()
-
-
-accelerator = accelerate.Accelerator()
-device = accelerator.device
 config = OmegaConf.load(f"{config}")
-model = load_model_from_config(config, f"{ckpt}", device=device)
+model = load_model_from_config(config, f"{ckpt}", device="cuda")
 
 
 def fits_in_batch(current_jobs, new_job):
